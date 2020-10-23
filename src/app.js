f76637794dddc582fecc17ac46e12140e173c3c7
Amidery <zhenev.eva.me@gmail.com>
2020-10-23 21:25:49 +0300

    updated main logic, added auto update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import onChange from 'on-change';
 import axios from 'axios';
+import _ from 'lodash';
 import * as yup from 'yup';
 import i18next from 'i18next';
-import { renderInput, renderFeed, renderMessage } from './view';
+import { renderToggle, renderInput, renderFeed, renderMessage } from './view';
 
 const urlInput = document.getElementById('urlInput');
 const urlSubmit = document.getElementById('urlSubmitButton');
@@ -16,56 +17,50 @@ const parse = (xml) => {
   const posts = [...dom.getElementsByTagName('item')].map((post) => {
     const postTitle = post.querySelector('title').textContent;
     const postLink = post.querySelector('link').textContent;
-    return { postTitle, postLink };
+    const postId = _.uniqueId();
+    return { postTitle, postLink, postId };
   });
 
-  return { feedTitle, feedDescription, posts };
+  const id = _.uniqueId();
+
+  return { feedTitle, feedDescription, posts, id };
 };
 
 const app = () => {
-  urlSubmit.value = i18next.t('button');
-
   const state = {
+    lng: 'en',
     userurl: '',
     isValid: 'valid',
-    message: 'validated',
-    processStatus: '',
-    feedURLs: [],
-    feedList: [],
-  };
-
-  const processStatuses = {
-    validated: (watchedState) => {
-      renderMessage(watchedState);
-      urlSubmit.disabled = true;
-    },
-    added: (watchedState) => {
-      renderMessage(watchedState);
-      urlSubmit.disabled = false;
-    },
-    failed: (watchedState) => {
-      renderMessage(watchedState);
-      urlSubmit.disabled = false;
-    },
+    feed: [],
+    message: '',
+    status: '',
   };
 
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
+      case 'lng':
+        renderToggle(value);
+        break;
       case 'isValid':
         renderInput(value);
         break;
-      case 'processStatus':
-        processStatuses[value](watchedState);
-        break;
-      case 'feedList':
+      case 'feed':
         renderFeed(value);
+        break;
+      case 'message':
+        renderMessage(watchedState);
+        break;
+      case 'status':
+        if (value === 'updated') {
+          renderFeed(watchedState.feed);
+        }
         break;
       default:
         break;
     }
   });
 
-  const isDouble = (userurl) => watchedState.feedURLs.includes(userurl);
+  const isDouble = (userurl) => watchedState.feed.map((channel) => channel.url).includes(userurl);
 
   const isUrlValid = (userurl) => {
     const schema = yup.string().url();
@@ -75,39 +70,98 @@ const app = () => {
       .then((valid) => valid);
   };
 
-  urlSubmit.addEventListener('click', () => {
-    watchedState.userurl = urlInput.value;
+  const updateChannel = (channel) => {
+    const channelIndex = watchedState.feed.map((channelToUpdate, index) => (channelToUpdate.url === channel.url ? index : ''))
+      .filter((v) => v !== '');
 
-    isUrlValid(watchedState.userurl)
+    axios.get(`https://cors-anywhere.herokuapp.com/${channel.url}`)
+      .then((response) => {
+        const updChannel = parse(response);
+        const updPosts = updChannel.posts;
+        const currPosts = channel.posts;
+        const newPosts = _.differenceBy(updPosts, currPosts, 'postLink');
+        if (newPosts.length > 0) {
+          watchedState.feed[channelIndex].posts = [...currPosts, ...newPosts];
+          watchedState.status = 'updated';
+        }
+      })
+      .catch((error) => {
+        watchedState.status = 'failed';
+
+        if (error.response) {
+          watchedState.message = `${i18next.t('errorResponseAuto')}${error.response.status}`;
+        } else if (error.request) {
+          watchedState.message = `${i18next.t('errorRequestAuto')}${error.request}`;
+        } else {
+          watchedState.message = `${i18next.t('errorAuto')}${error.message}`;
+        }
+      })
+      .then(() => {
+        watchedState.status = '';
+        setTimeout(updateChannel, 5000, watchedState.feed[channelIndex]);
+      });
+  };
+
+  const buttonEN = document.getElementById('en');
+  const buttonRU = document.getElementById('ru');
+
+  buttonEN.addEventListener('click', () => {
+    i18next.changeLanguage('en');
+    watchedState.lng = 'en';
+    urlSubmit.value = i18next.t('button');
+  });
+  buttonRU.addEventListener('click', () => {
+    i18next.changeLanguage('ru');
+    watchedState.lng = 'ru';
+    urlSubmit.value = i18next.t('button');
+  });
+
+  urlSubmit.addEventListener('click', () => {
+    const userurl = urlInput.value;
+
+    isUrlValid(userurl)
       .then((valid) => {
+        urlSubmit.disabled = true;
         if (!valid) {
           throw i18next.t('invalid');
         }
       })
       .then(() => {
-        if (isDouble(watchedState.userurl)) {
+        if (isDouble(userurl)) {
           throw i18next.t('double');
         }
       })
       .then(() => {
-        axios.get(`https://cors-anywhere.herokuapp.com/${watchedState.userurl}`)
-          .then((feed) => {
-            const parsedFeed = { ...parse(feed), url: watchedState.userurl };
-            console.log(parsedFeed);
-            watchedState.feedList.push(parsedFeed);
+        axios.get(`https://cors-anywhere.herokuapp.com/${userurl}`)
+          .then((response) => {
+            const parsedChannel = { ...parse(response), url: userurl };
+            watchedState.feed.push(parsedChannel);
 
-            watchedState.isValid = '';
             urlInput.value = '';
-            watchedState.message = i18next.t('success');
-            watchedState.processStatus = 'added';
             urlSubmit.disabled = false;
-            watchedState.feedURLs.push(watchedState.userurl);
+            watchedState.status = 'success';
+            watchedState.message = i18next.t('success');
+
+            setTimeout(updateChannel, 5000, parsedChannel);
+          })
+          .catch((error) => {
+            urlSubmit.disabled = false;
+            watchedState.status = 'failed';
+
+            if (error.response) {
+              watchedState.message = `${i18next.t('errorResponse')}${error.response.status}`;
+            } else if (error.request) {
+              watchedState.message = error.request;
+            } else {
+              watchedState.message = error.message;
+            }
           });
       })
-      .catch((errorMessage) => {
-        watchedState.message = errorMessage;
+      .catch((error) => {
+        urlSubmit.disabled = false;
+        watchedState.status = 'failed';
         watchedState.isValid = 'invalid';
-        watchedState.processStatus = 'failed';
+        watchedState.message = error;
       });
   });
 };
@@ -123,6 +177,10 @@ export default () => {
           double: 'This URL already exists in your feed',
           invalid: 'Sorry, your url is invalid.',
           button: 'Add to your feed',
+          errorResponse: 'Error: ',
+          errorResponseAuto: 'An unexpected error has occured while trying to autoupdate the feed. Error: ',
+          errorRequestAuto: 'An unexpected error has occured while trying to autoupdate the feed. ',
+          errorAuto: 'An unexpected error has occured while trying to autoupdate the feed. ',
         },
       },
       ru: {
@@ -131,19 +189,13 @@ export default () => {
           double: 'Этот URL уже есть в вашей ленте новостей',
           invalid: 'Введен некорректный URL.',
           button: 'Добавить в вашу ленту',
+          errorResponse: 'Ошибка: ',
+          errorResponseAuto: 'Произошла ошибка при обновлении. Ошибка: ',
+          errorRequestAuto: 'Произошла ошибка при обновлении. ',
+          errorAuto: 'Произошла ошибка при обновлении. ',
         },
       },
     },
   })
     .then(() => app());
-
-  urlSubmit.value = i18next.t('button');
-
-  const buttonEN = document.getElementById('en');
-  const buttonRU = document.getElementById('ru');
-
-  buttonEN.addEventListener('click', () => i18next.changeLanguage('en'));
-  buttonRU.addEventListener('click', () => i18next.changeLanguage('ru'));
-
-  i18next.on('languageChanged', () => app());
 };
--- src/view.js
+++ src/view.js
@@ -1,3 +1,20 @@
+const renderToggle = (value) => {
+  const buttonEN = document.getElementById('en');
+  const buttonRU = document.getElementById('ru');
+
+  if (value === 'ru') {
+    buttonRU.classList.remove('btn-secondary');
+    buttonRU.classList.add('btn-warning');
+    buttonEN.classList.remove('btn-warning');
+    buttonEN.classList.add('btn-secondary');
+  } else {
+    buttonRU.classList.add('btn-secondary');
+    buttonRU.classList.remove('btn-warning');
+    buttonEN.classList.add('btn-warning');
+    buttonEN.classList.remove('btn-secondary');
+  }
+};
+
 const renderInput = (value) => {
   const urlInput = document.getElementById('urlInput');
 
@@ -8,38 +25,11 @@ const renderInput = (value) => {
   }
 };
 
-const renderFeed = (feedlist) => {
-  const lastChannel = feedlist[feedlist.length - 1];
-
-  const newchannel = document.createElement('div');
-  const channelTitle = document.createElement('h2');
-  const channelDescription = document.createElement('p');
-
-  const { feedTitle, feedDescription } = lastChannel;
-  channelTitle.textContent = feedTitle;
-  channelDescription.textContent = feedDescription;
-
-  newchannel.classList.add('d-flex', 'flex-column', 'col-sm-8', 'mt-4');
-  newchannel.append(channelTitle);
-  newchannel.append(channelDescription);
-
-  lastChannel.posts.map((obj) => {
-    const link = document.createElement('a');
-    const { postTitle, postLink } = obj;
-    link.setAttribute('href', postLink);
-    link.textContent = postTitle;
-    newchannel.append(link);
-  });
-
-  const feed = document.getElementById('feed');
-  feed.append(newchannel);
-};
-
 const renderMessage = (state) => {
   const message = document.getElementById('message');
   message.textContent = state.message;
 
-  if (state.processStatus === 'failed') {
+  if (state.status === 'failed') {
     message.classList.remove('text-success', 'bg-white');
     message.classList.add('text-danger', 'bg-white');
   } else {
@@ -48,7 +38,60 @@ const renderMessage = (state) => {
   }
 };
 
+const renderFeed = (feed) => {
+  feed.map((channel) => {
+    console.log(channel);
+    const hasChannel = document.getElementById(channel.id);
+    if (hasChannel) {
+      channel.posts.map((post) => {
+        const hasPost = document.getElementById(post.postId);
+        if (hasPost === null) {
+          const link = document.createElement('a');
+          const { postTitle, postLink, postId } = post;
+          link.setAttribute('href', postLink);
+          link.textContent = postTitle;
+          link.id = postId;
+
+          const currPosts = hasChannel.querySelector('div');
+          currPosts.prepend(link);
+        }
+      });
+      return;
+    }
+
+    const newchannel = document.createElement('div');
+    newchannel.classList.add('col-sm-8', 'mt-4');
+
+    const channelTitle = document.createElement('h2');
+    const channelDescription = document.createElement('p');
+
+    const { feedTitle, feedDescription, id } = channel;
+    channelTitle.textContent = feedTitle;
+    channelDescription.textContent = feedDescription;
+    newchannel.id = id;
+
+    const channelPosts = document.createElement('div');
+    channel.posts.map((obj) => {
+      const link = document.createElement('a');
+      const { postTitle, postLink, postId } = obj;
+      link.setAttribute('href', postLink);
+      link.textContent = postTitle;
+      link.id = postId;
+      channelPosts.append(link);
+    });
+
+    channelPosts.classList.add('d-flex', 'flex-column');
+    newchannel.append(channelTitle);
+    newchannel.append(channelDescription);
+    newchannel.append(channelPosts);
+
+    const newFeed = document.getElementById('feed');
+    newFeed.prepend(newchannel);
+  });
+};
+
 export {
+  renderToggle,
   renderInput,
   renderFeed,
   renderMessage,
